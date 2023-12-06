bc48aaa75c37dcef61e49498576617b32b343a30
Morski <morski20025@gmail.com>
2023-12-06 22:34:48 +0300

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    @vite(['resources/scss/app.scss', 'resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/scss/app.scss', 'resources/js/app.js'])
     <title>Добро пожаловать | ВКонтакте</title>
 </head>
 <body>
